fed24a593072da50ace09639ead594d8479fade5
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2023-10-06 22:00:03 +0800

    ref: update TaskOptions.vue

--- resources/js/components/TaskOptions.vue
+++ resources/js/components/TaskOptions.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, toRefs} from "vue";
+import {ref, toRefs, watch} from "vue";
 import {useFormStore} from "../store/formStore.js";
 
 const props = defineProps({
@@ -10,40 +10,34 @@ const props = defineProps({
 })
 const { task } = toRefs(props)
 const formStore = useFormStore();
-const statuses = [
-    {key: 'Todo', value: 'todo'},
-    {key: 'In Progress', value: 'inprogress'},
-    {key: 'Completed', value: 'completed'},
-];
 const status = ref();
-function handleChangeStatus(item) {
-    status.value = item
+watch(status, handleChangeStatus)
+
+function handleChangeStatus() {
+    formStore?.handleFormState(false, 'update', {
+        ...task?.value,
+        status: status.value
+    });
+    formStore?.handleFormSubmit();
 }
 </script>
 
 <template>
     <div class="options-container">
         <div class="move-to-item">
-            <v-btn
-                id="menu-activator"
-                variant="tonal"
-                class="button main-button h-7 text-left uppercase"
-                width="100%"
-            >
-                MOVE TO {{ status?.key }}
-            </v-btn>
-            <v-menu activator="#menu-activator">
-                <v-list>
-                    <v-list-item
-                        v-for="(item) in statuses"
-                        :key="item.key"
-                        :value="item.value"
-                        @click="handleChangeStatus(item)"
-                    >
-                        <v-list-item-title>{{ item.key }}</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
+            <v-label>Move to</v-label>
+            <div class="flex gap-1" v-show="task.status !== 'todo'">
+                <input type="radio" id="todo" value="todo" v-model="status" name="status" :disabled="formStore.isFormSubmitting" />
+                <label for="todo">Todo</label>
+            </div>
+            <div class="flex gap-1" v-show="task.status !== 'inprogress'">
+                <input type="radio" id="inprogress" value="inprogress" v-model="status" name="status" :disabled="formStore.isFormSubmitting" />
+                <label for="inprogress">In Progress</label>
+            </div>
+            <div class="flex gap-1" v-show="task.status !== 'completed'">
+                <input type="radio" id="completed" value="completed" v-model="status" name="status" :disabled="formStore.isFormSubmitting" />
+                <label for="completed">Completed</label>
+            </div>
         </div>
         <ul>
             <li><button @click="formStore.handleFormState(true, 'update', task)">Edit</button></li>
@@ -56,15 +50,15 @@ function handleChangeStatus(item) {
 @import "./../../css/app.css";
 
 .options-container {
-    @apply border border-slate-300 rounded-md py-3 absolute bg-white shadow-md right-0 top-0 w-52;
+    @apply border border-slate-300 rounded-md py-3 absolute bg-white shadow-md right-0 top-0 w-full;
 
     .move-to-item {
-        @apply flex justify-between p-2 pt-0 w-full text-xs;
+        @apply flex justify-between px-3 pt-0 pb-3 w-full text-xs border-b border-b-slate-200;
     }
 
     ul {
         li {
-            @apply hover:bg-slate-100 px-2 py-1;
+            @apply hover:bg-slate-100 px-4 py-1;
 
             button {
                 @apply w-full text-left;
