6e24b5ed6d81616c4c17ab546e2570d95a3cd1de
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2023-10-06 19:19:47 +0800

    feat: display empty task

--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -20,6 +20,9 @@ const { status, tasks } = toRefs(props);
     <div class="task-list">
         <h3 class="list-label">{{ status }}</h3>
         <div class="tasks-container">
+            <div v-if="tasks.length === 0" class="empty-list">
+                <span>You do not have task here...</span>
+            </div>
             <Task v-for="task in tasks" :key="task.id" :task="task" />
         </div>
     </div>
@@ -37,7 +40,11 @@ const { status, tasks } = toRefs(props);
     }
 
     .tasks-container {
-        @apply p-3 flex flex-col gap-5
+        @apply p-3 flex flex-col gap-5;
+
+        .empty-list {
+            @apply border border-dashed border-slate-300 rounded-md text-center p-2 text-slate-400;
+        }
     }
 }
 </style>
